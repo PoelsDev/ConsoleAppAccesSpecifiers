2dd0d35c1e53a7cb3ab253071f74d324c48db1d6
ThibaultP-immalle <31651240+ThibaultP-immalle@users.noreply.github.com>
2018-03-27 13:05:56 +0200

    Program

--- ConsoleApAccesSpecifiers/Program.cs
+++ ConsoleApAccesSpecifiers/Program.cs
@@ -2,11 +2,30 @@
 
 namespace ConsoleApAccesSpecifiers
 {
+    class Tafel
+    {
+        int aantalPoten = 2;
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("De tafel heeft {0} poten", aantalPoten);
+        }
+    }
+}
+
+namespace MeubelWinkel
+{
+    using ConsoleApAccesSpecifiers;
+
     class Program
     {
-        static void Main(string[] args)
+
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            ConsoleApAccesSpecifiers.Tafel t1 = new ConsoleApAccesSpecifiers.Tafel();
+            Tafel t2 = new Tafel();
+            t1.PrintInfo();
+            t2.PrintInfo();
         }
     }
 }
